5d41915783592759495c53a8124c7edbbb1851df
Wiktor Wojnicz <w2002wiktor@o2.pl>
2026-05-30 19:00:00 +0200

    nowy wyglad

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './styles.css'
 import App from './App.jsx'
 
 // --- GLOBALNY INTERCEPTOR SIECIOWY (FAIL-SAFE FOR JWT EXPIRATION) ---
